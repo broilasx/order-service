dd21aa870daf516376012f65bc50b0c7c17119f8
Bruno Almeida <brunoalmeida7426@gmail.com>
2025-03-25 21:02:47 +0000

    Created the entity files

---
+++ src/main/java/com/buildrun/orderms/entity/OrderItem.java
@@ -0,0 +1,37 @@
+package com.buildrun.orderms.entity;
+
+import java.math.BigDecimal;
+
+public class OrderItem {
+
+    private String product;
+    private Integer quantity;
+    private BigDecimal price;
+
+    public OrderItem() {
+    }
+    
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
